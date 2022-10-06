e48696aefb889ab34e5497a649ffbe49937b7ae5
yantavares <yantdo1@gmail.com>
2022-10-06 14:04:47 -0300

    ajeitei html

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from ..secretKey import generateKey
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = generateKey()
+SECRET_KEY = "jsidaodhnaidnafjnainfaiofniefn"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
